b8d070f87ef7882ffc717d5e20223ea773d82bc1
arifquuu <arifqukeren@gmail.com>
2025-08-28 22:41:04 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+
+    <h1>Halo, selamat datang di Index yang tersedia</h1>
+
+    <p>Klik dibawah untuk lanjut ke Index yang ada</p>
+
+    <a href="index.php">Go to Index Page</a>
+
+</body>
+</html>
